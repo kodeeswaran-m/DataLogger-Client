54a98a724b995d8b08ba25b97c825a3387f0563f
kodeeswaran <kodeeswaran.manju@aspiresys.com>
2025-11-20 15:10:10 +0530

    added reusable snackbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { SnackbarProvider } from "./context/SnackbarContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </BrowserRouter>
 );
